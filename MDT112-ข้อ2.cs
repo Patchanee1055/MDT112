e8390dab64f06243cb45020f41860eb899fa3195
LAPTOP-TTU2QOKA\wwwpa <patchanee.spk@mail.kmutt.ac.th>
2021-03-28 21:01:18 +0700

    สอบ-ข้อ2

---
+++ MDT112-ข้อ2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,b,x,y;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            x = a;
+            y = b;
+            if (x == y)
+            {
+                Console.Write(x);
+            }
+            else if (x < y) 
+            { x = x + a; 
+            }
+            else { y = y + b; }
+        }
+    }
+}
